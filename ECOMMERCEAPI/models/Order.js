41cae7669b061a5107d3cf7080208e639a6d0c9d
hxxone <hwlee0530@naver.com>
2021-10-30 15:57:53 +0900

    CRUD 구현

--- ECOMMERCEAPI/models/Order.js
+++ ECOMMERCEAPI/models/Order.js
@@ -2,10 +2,10 @@ const mongoose = require("mongoose");
 
 const OrderSchema = new mongoose.Schema(
 	{
-		userId: { type: String, required: true, unique: true },
-		product: [
-			{ productId: { type: String } },
-			{ quantity: { type: Number, default: 1 } },
+		userId: { type: String, required: true},
+		products: [
+			{ productId: { type: String },
+			quantity: { type: Number, default: 1 } },
 		],
 		amount: { type: Number, required: true },
 		address: { type: Object, required: true },
